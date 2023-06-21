cef21ea19ae096ae9d2c82f9ba7c0b5aadd9132d
Valentin MEROU <valentin.merou@gmail.com>
2023-06-22 00:58:02 +0200

    correction de mini-fautes

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_233641) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["family_id"], name: "index_children_on_family_id"
+  end
 
   create_table "families", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -45,13 +46,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_17_233641) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "husband_name"
-    t.string "wife_name"
-    t.string "address"
-    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "children", "families"
   add_foreign_key "families", "users"
 end
