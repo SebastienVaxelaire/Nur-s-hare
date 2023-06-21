02bb9088c24d63891494c874e90d87fcfbf44532
sebastien vaxelaire <svaxelaire@fluence.eu>
2023-06-21 19:53:07 +0200

    delete a double validates of gender

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,6 +2,5 @@ class Child < ApplicationRecord
   belongs_to :family
   validates :gender, presence: true
   validates :birthday, presence: true
-  validates :gender, presence: true
   has_one_attached :photo
 end
