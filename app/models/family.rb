722cac8be01a4ad90937c9ce6c7d15e2f7c7eb99
Valentin MEROU <valentin.merou@gmail.com>
2023-06-22 23:53:39 +0200

    ajout validation téléphone, autocomplete procisoire et front show famille

--- app/models/family.rb
+++ app/models/family.rb
@@ -5,7 +5,7 @@ class Family < ApplicationRecord
   validates :name, presence: true, on: :update
   validates :description, presence: true, on: :update, length: { minimum: 10, maximum: 1000 }
   validates :address, presence: true, on: :update
-  validates :phone_number, presence: true, on: :update
+  validates :phone_number, presence: true, on: :update, format: { with: /\A(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})\z/, message: "Le numéro de téléphone n'est pas exact"}
   validate :validate_husband_or_wife_first_name_presence, on: :update
   has_many_attached :photos
 
