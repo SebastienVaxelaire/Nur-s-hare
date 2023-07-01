a39291e4191cfc4005be9a99982597b2561ce23e
sebastien vaxelaire <svaxelaire@fluence.eu>
2023-07-01 09:21:14 +0200

    fix bug and continue style

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,7 +3,7 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
@@ -11,16 +11,17 @@ export default class extends Controller {
       { channel: 'ChatroomChannel', id: this.chatroomIdValue },
       { received: data => this.#insertMessageAndScrollDown(data) }
     );
-    this.#scrollToBottom()
+    this.#scrollToBottom();
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`);
     // console.log(this.messagesTarget);
   }
 
 
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-    this.#scrollToBottom()
   }
 
   resetForm(event) {
@@ -34,5 +35,23 @@ export default class extends Controller {
   disconnect() {
     console.log("Unsubscribed from the chatroom")
     this.channel.unsubscribe()
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
