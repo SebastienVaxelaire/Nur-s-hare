410d5e23d12dc157438652b9f8763a2efa38e26d
Valentin MEROU <valentin.merou@gmail.com>
2023-07-07 22:58:25 +0200

    problème de new_group_planning_path non résolu... à l'aide svp

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
       resources :messages, only: :create
     end
     resources :families_groups, only: [:new, :create, :destroy]
-    resources :families_groups, only: [:new, :create]
     resources :plannings, only: [:new, :create]
     resources :events do
       member do
